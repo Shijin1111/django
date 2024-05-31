892d375c00bce0462c8cbe3dd72e6f3b44cf540f
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-06-01 00:24:19 +0530

    second excersise starting

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email =models.CharField(max_length=50,unique=True)
+    def __str__(self):
+        return self.email
