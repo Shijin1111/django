166d08dd6d4b2e87773506ece7675140ee2c9255
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-06-05 08:32:29 +0530

    starting validation

--- pro2/app2/views.py
+++ pro2/app2/views.py
@@ -7,4 +7,11 @@ def index(request):
 
 def form_name_view(request):
     form = forms.Form1()
+    if request.method=='POST':
+        form = forms.Form1(request.POST)
+        if form.is_valid():
+            print("validation success")
+            print('name:',form.cleaned_data['name'])
+            print('email',form.cleaned_data['email'])
+            print('text',form.cleaned_data['text'])
     return render(request,'app2/formpage.html',{'form':form})
