8c355c915d7a1a57b52aa4deedfbb4b858b7496c
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-06-08 06:13:01 +0530

    level 4 completed

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR/"templates"
+STATIC_DIR = BASE_DIR/"static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+         STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
