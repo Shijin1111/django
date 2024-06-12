01c6c2d23d687fe5f4d92c81e305e2cd2e134743
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-06-12 23:37:36 +0530

    login

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from blog.models import Post,Comments
+from blog.models import Post,Comment
 
 class PostForm(forms.ModelForm):
     class Meta():
@@ -13,7 +13,7 @@ class PostForm(forms.ModelForm):
         
 class CommentForm(forms.ModelForm):
     class Meta():
-        model = Comments
+        model = Comment
         fields = ('author','text')
     
         widgets = {
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+    path('accounts/login/',auth_views.LoginView,name="login"),
+    path('accounts/logout/',auth_views.LogoutView,name="logout",kwargs={'next_page':'/'}),
 ]
