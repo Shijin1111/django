2992ccf5346c22c6bf19cf705b2d8897a2d558bc
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-06-16 09:05:45 +0530

    mysite completed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -41,7 +41,7 @@ class DraftListView(LoginRequiredMixin,ListView):
     redirect_field_name = 'blog/post_list.html'
     model = Post
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
     
 #####################################################################################
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('accounts/login/',auth_views.LoginView,name="login"),
-    path('accounts/logout/',auth_views.LogoutView,name="logout",kwargs={'next_page':'/'}),
+    path('accounts/login/',auth_views.LoginView.as_view(),name="login"),
+    path('accounts/logout/',auth_views.LogoutView.as_view(),name="logout",kwargs={'next_page':'/'}),
 ]
